0693c241908c2cac4fe2b69e0a9ebae770e440f4
aridotdev <aridotdev@gmail.com>
2025-09-18 10:18:01 +0700

    initial commit

--- app/app.vue
+++ app/app.vue
@@ -1,34 +1,36 @@
 <script setup lang="ts">
-const colorMode = useColorMode()
+const colorMode = useColorMode();
 
-const color = computed(() => colorMode.value === 'dark' ? '#1b1718' : 'white')
+const color = computed(() =>
+  colorMode.value === "dark" ? "#1b1718" : "white"
+);
 
 useHead({
   meta: [
-    { charset: 'utf-8' },
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-    { key: 'theme-color', name: 'theme-color', content: color }
+    { charset: "utf-8" },
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+    { key: "theme-color", name: "theme-color", content: color },
   ],
-  link: [
-    { rel: 'icon', href: '/favicon.ico' }
-  ],
+  link: [{ rel: "icon", href: "/favicon.png" }],
   htmlAttrs: {
-    lang: 'en'
-  }
-})
+    lang: "en",
+  },
+});
 
-const title = 'Nuxt Dashboard Template'
-const description = 'A professional dashboard template built with Nuxt UI, featuring multiple pages, data visualization, and comprehensive management capabilities for creating powerful admin interfaces.'
+const title = "QRCC Data Center";
+const description =
+  "Sebuah aplikasi dashboard berbasis web yang berisikan data-data yang berkaitan dengan departemen QRCC termasuk PPM, F-Cost, Sales, Quality Issue dan lain-lain";
 
 useSeoMeta({
   title,
   description,
   ogTitle: title,
   ogDescription: description,
-  ogImage: 'https://ui4.nuxt.com/assets/templates/nuxt/dashboard-light.png',
-  twitterImage: 'https://ui4.nuxt.com/assets/templates/nuxt/dashboard-light.png',
-  twitterCard: 'summary_large_image'
-})
+  ogImage: "https://ui4.nuxt.com/assets/templates/nuxt/dashboard-light.png",
+  twitterImage:
+    "https://ui4.nuxt.com/assets/templates/nuxt/dashboard-light.png",
+  twitterCard: "summary_large_image",
+});
 </script>
 
 <template>
