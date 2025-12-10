1f2bdfc2f57d27600e56f8df8c44f0742f7105a8
aridotdev <aridotdev@gmail.com>
2025-12-10 16:48:22 +0700

    ui: add form new category

--- app/pages/settings/category.vue
+++ app/pages/settings/category.vue
@@ -1,4 +1,25 @@
 <script setup lang="ts">
+import * as z from 'zod';
+import type { FormSubmitEvent } from '@nuxt/ui';
+
+const schema = z.object({
+  name: z.string('Category name is required').max(20, 'Maximum 25 characters')
+})
+
+type Schema = z.output<typeof schema>;
+
+const category = reactive<Partial<Schema>>({
+  name: undefined
+})
+
+const toast = useToast();
+
+async function onSubmit(event: FormSubmitEvent<Schema>) {
+  toast.add({ title: 'Success', description: 'The form has been submitted. ' + category.name?.toUpperCase(), color: 'success' })
+  console.log(event.data.name.toUpperCase())
+  category.name = undefined;
+}
+
 const { data } = useFetch('/api/category', {
   lazy: true
 })
@@ -9,6 +30,17 @@ const { data } = useFetch('/api/category', {
     <div class="text-right mb-4">
       <UButton icon="i-lucide-plus" class="font-bold">Add Category</UButton>
     </div>
+
+    <UForm :schema="schema" :state="category" class="flex items-end gap-1 mb-4" @submit="onSubmit">
+      <UFormField label="Category Name" name="name" class="flex-1">
+        <UInput v-model="category.name" class="w-full" />
+      </UFormField>
+
+      <UButton type="submit">
+        Submit
+      </UButton>
+    </UForm>
+
     <div class="border border-b border-default rounded-lg p-2">
       <UTable :data="data" class="shrink-0" :ui="{
         base: 'table-fixed border-separate border-spacing-0',
