0c68114e53556ac15c1499e0b1ae23085cdff478
aridotdev <aridotdev@gmail.com>
2025-12-10 12:16:45 +0700

    feat: add style table category and Add Category button

--- app/pages/settings/category.vue
+++ app/pages/settings/category.vue
@@ -5,6 +5,18 @@ const { data } = useFetch('/api/category', {
 </script>
 
 <template>
-  <h1>Category Product Page</h1>
-  <UTable :data="data" class="flex-1" />
+  <main>
+    <div class="text-right mb-4">
+      <UButton icon="i-lucide-plus" class="font-bold">Add Category</UButton>
+    </div>
+    <div class="border border-b border-default rounded-lg p-2">
+      <UTable :data="data" class="shrink-0" :ui="{
+        base: 'table-fixed border-separate border-spacing-0',
+        thead: '[&>tr]:bg-elevated/50 [&>tr]:after:content-none',
+        tbody: '[&>tr]:last:[&>td]:border-b-0',
+        th: 'first:rounded-l-lg last:rounded-r-lg border-y border-default first:border-l last:border-r',
+        td: 'border-b border-default'
+      }" />
+    </div>
+  </main>
 </template>
