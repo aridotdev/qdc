2a42849d281f06c751a573ae2f3191d3f98fabb9
arisafaribinridwan <arisafari.ridwan@gmail.com>
2026-09-23 22:40:04 +0700

    fix(TASK-001): typecheck and lint error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,13 @@
-import { compileScript } from "vue/compiler-sfc";
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-
   typescript: {
     tsConfig: {
       compilerOptions: {
-        strict: true,
-        typecheck: true,
+        strict: true
       }
     }
   },
-
   modules: ["@nuxt/eslint"]
 })
